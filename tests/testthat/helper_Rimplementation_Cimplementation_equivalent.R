ae4f302c2f89c02aaf61700587e45e9cad4bb960
Mirko Reul <public@reul.ws>
2020-06-30 20:06:55 +0200

    Remove interface tests temporarily

--- tests/testthat/helper_Rimplementation_Cimplementation_equivalent.R
+++
@@ -1,8 +0,0 @@
-# Data
-
-data("Social_Evolution")
-callNetwork <- defineNetwork(nodes = actors, directed = TRUE)
-callNetwork <- linkEvents(x = callNetwork, changeEvent = calls, nodes = actors)
-callsDependent <- defineDependentEvents(events = calls, nodes = actors, defaultNetwork = callNetwork)
-
-# parameterRemOrderedRImplementaion = c(6.1992539183744,1.43600453526761,0.164126783806186)
