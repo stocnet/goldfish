42deb067f0d6d9adc340e57cfc1439fc9b2ad774
ualvaro <alvaro.uzaheta@gess.ethz.ch>
2022-07-13 16:15:19 +0200

    clean objects before second vignette is compiled

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -15,6 +15,8 @@ library(knitr)
 knit("teaching1.Rmd.orig", "teaching1.Rmd")
 purl("teaching1.Rmd.orig", "teaching1.R")
 
+rm(list = ls())
+
 knit("teaching2.Rmd.orig", "teaching2.Rmd")
 purl("teaching2.Rmd.orig", "teaching2.R")
 
