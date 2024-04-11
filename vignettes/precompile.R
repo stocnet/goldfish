b3d76bf7c3c427cc83bcbbff55b969803921a265
ualvaro <alvaro.uzaheta@gess.ethz.ch>
2024-04-11 03:28:47 +0200

    Creates `README.Rmd`, update GitHub action accordingly, precompile includes the README rendering

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,27 +1,53 @@
 # Precompiled vignettes
+# Adapted from bcgov/bcdata/vignettes/precompile.R under Apache License 2.0
 
-# delete old figures
-unlink("vignettes/teaching", recursive = TRUE)
 
-# install packages when GitHub action
+# Precompile vignettes -------------------------------------------------------
 
-# compile vignette and generate R files
-setwd("vignettes/")
+precompile <- function(vignette_to_run = NULL) {
+  orig_files <- file.path(
+    tools::list_files_with_exts(
+      dir = "vignettes",
+      exts = "Rmd\\.orig",
+      full.names = TRUE
+    )
+  )
 
-library(knitr)
+  if (!is.null(vignette_to_run)) {
+    orig_files <- orig_files[basename(orig_files) %in% vignette_to_run]
 
-knit("teaching1.Rmd.orig", "teaching1.Rmd")
-purl("teaching1.Rmd.orig", "teaching1.R")
+    if (rlang::is_empty(orig_files)) stop("Not a vignette!")
+  }
 
-rm(list = ls())
+  ## Convert *.orig to *.Rmd -------------------------------------------------
+  purrr::walk(
+    orig_files,
+    \(x) {
+      rmdFile <- tools::file_path_sans_ext(x)
+      knitr::knit(x, rmdFile, envir = globalenv())
+      purlFile <- paste0(tools::file_path_sans_ext(rmdFile), ".R")
+      knitr::purl(x, purlFile)
+    }
+  )
 
-knit("teaching2.Rmd.orig", "teaching2.Rmd")
-purl("teaching2.Rmd.orig", "teaching2.R")
+  ## Move .png files into correct directory so they render -------------------
+  images <- file.path(list.files("teaching", pattern = "plot-.+\\.png$"))
+  success <- file.copy(
+    from = file.path("teaching", images),
+    to = file.path("vignettes/teaching/", images),
+    overwrite = TRUE
+  )
 
-rm(list = ls())
+  ## Clean up if successful --------------------------------------------------
+  if (!all(success)) {
+    stop("Image files were not successfully transferred to vignettes directory")
+  } else {
+    unlink("teaching", recursive = TRUE)
+  }
+}
 
-knit("dynami-example.Rmd.orig", "dynami-example.Rmd")
-purl("dynami-example.Rmd.orig", "dynami-example.R")
+## Run all vignettes
+precompile()
 
 
 # check if the RMD files contain errors
@@ -31,10 +57,18 @@ lapply(
     text <- readLines(x)
     haveErrors <- grepl("Error:", text)
     if (any(haveErrors)) {
-      paste("Error in", x, " on line",
-            paste(which(haveErrors), collapse = ", "))
+      paste(
+        "Error in", x, " on line",
+        paste(which(haveErrors), collapse = ", ")
+      )
     }
   }
 )
 
-setwd("..")
+# render README.Rmd
+rmarkdown::render(
+  "README.Rmd",
+  output_format = "md_document",
+  envir = globalenv()
+)
+
